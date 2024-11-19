a32d76dde9c0af0b78e964aae30873d475bbe707
Utsav Sangani <utsavsangani444@gmail.com>
2024-11-19 16:05:33 +0530

    Updated the portfolio

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+// Function to close a modal
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+// Close the modal when clicking outside of it
+window.onclick = function(event) {
+    const modals = document.getElementsByClassName("modal");
+    Array.from(modals).forEach((modal) => {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+};
